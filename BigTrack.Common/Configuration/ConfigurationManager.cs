94bd79c2fd3e0bcaedcec58231018e422cb3dabd
Alex <AlexTheProgrammerMail@gmail.com>
2016-07-03 21:06:44 -0700

    conf.manager singleton

--- BigTrack.Common/Configuration/ConfigurationManager.cs
+++ BigTrack.Common/Configuration/ConfigurationManager.cs
@@ -4,7 +4,24 @@ namespace BigTrack.Common.Configuration
 {
 	public class ConfigurationManager
 	{
-		public static ConfigurationManager Instanse { get; set; }
+		private ConfigurationManager() { }
+		private static readonly object locker = new object();
+		private static ConfigurationManager instanse;
+
+		public static ConfigurationManager Instanse
+		{
+			get
+			{
+				if (instanse == null)
+					lock (locker)
+					{
+						if (instanse == null)
+							instanse = new ConfigurationManager();
+					}
+
+				return instanse;
+			}
+		}
 
 		public List<DatabaseConfiguration> GetDatabaseConfigurations()
 		{
