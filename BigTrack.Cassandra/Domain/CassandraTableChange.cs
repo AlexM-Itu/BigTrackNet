2860c8b8055d8d6e7b689e128b631220aaa51d51
Alex <AlexTheProgrammerMail@gmail.com>
2016-07-04 16:22:53 -0700

    Cassandra.Domain classes generated; refference to dtastax cassandra driiver

---
+++ BigTrack.Cassandra/Domain/CassandraTableChange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using BigTrack.Common.Domain;
+
+namespace BigTrack.Cassandra.Domain
+{
+	public class CassandraTableChange
+	{
+		public Guid Id { get; set; }
+		public HashSet<ColumnChange> ColumnChanges{ get; set; }
+		public string User { get; set; }
+		public Operations Operation { get; set; }
+		public string TableName { get; set; }
+		public DateTime Timestamp { get; set; }
+	}
+}
