932f89562a4b80f2b5c8cfce42144149418714d4
Alex <AlexTheProgrammerMail@gmail.com>
2016-07-03 23:52:29 -0700

    GetTableList po

--- BigTrack.Ui/Controllers/DatabaseController.cs
+++ BigTrack.Ui/Controllers/DatabaseController.cs
@@ -1,8 +1,30 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using BigTrack.Common.Configuration;
+using BigTrack.Ui.Models;
 
 namespace BigTrack.Ui.Controllers
 {
+	[RoutePrefix("database")]
     public class DatabaseController : ApiController
-    {
-    }
+	{
+		private readonly ConfigurationManager configurationManager = ConfigurationManager.Instanse;
+
+		[Route("{databaseId}/tables")]
+		public List<DatabaseTableListItemModel> GetTableList(string databaseId)
+		{
+			return configurationManager
+				.GetDatabaseManagerByDatabaseId(databaseId)
+				.GetDatabaseTables()
+				.Select(table => new DatabaseTableListItemModel
+				{
+					Id = table.Id,
+					Name = table.Name
+				})
+				.ToList();
+		}
+
+
+	}
 }
