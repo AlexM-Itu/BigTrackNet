e2b485eb4ceee1d92bb01eeb7be3e5772fac2a50
Alex <AlexTheProgrammerMail@gmail.com>
2016-07-04 18:36:04 -0700

    GetDatabaseTables; GetTableColumns implemeted

---
+++ BigTrack.Cassandra/Configuration/CassandraDialectDriver.cs
@@ -0,0 +1,28 @@
+﻿using BigTrack.Cassandra.Database;
+using BigTrack.Common.Database;
+
+namespace BigTrack.Cassandra.Configuration
+{
+	public class CassandraDialectDriver : IDialectDriver
+	{
+		public string DatabaseTypeName
+		{
+			get { return "Cassandra"; }
+		}
+
+		public string CatalogTypeName
+		{
+			get { return "table";  }
+		}
+
+		public bool UserTrackingSupported
+		{
+			get { return false; } // todo elaborate later
+		}
+
+		public IDatabaseManager DatabaseManager
+		{
+			get { return new CassandraDatabaseManager();}
+		}
+	}
+}
---
+++ BigTrack.Cassandra/Database/CassandraDatabaseManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BigTrack.Cassandra.Domain;
+using BigTrack.Common.Database;
+using BigTrack.Common.Domain;
+using Cassandra;
+using Cassandra.Mapping;
+
+namespace BigTrack.Cassandra.Database
+{
+	public class CassandraDatabaseManager : IDatabaseManager
+	{
+		private Cluster cluster;
+		private string connectionString;
+
+		public void SetConnectionString(string connectionString)
+		{
+			this.connectionString = connectionString;
+			cluster = Cluster.Builder().AddContactPoint(connectionString).Build();
+		}
+
+		private ISession GetSession()
+		{
+			return cluster.Connect(connectionString);
+		}
+
+		public List<Table> GetDatabaseTables()
+		{
+			using (var session = GetSession())
+			{
+				return session
+					.Execute("SELECT name FROM TableNames")
+					.Select(row => new Table
+					{
+						Id = row.GetValue<string>("name"),
+						Name = row.GetValue<string>("name")
+					})
+					.ToList();
+			}
+		}
+		
+		public List<string> GetTableColumns(string tableId)
+		{
+			using (var session = GetSession())
+			{
+				var query = session.Prepare("select  * from TableChanges where tableName = ?"); // todo replace*
+
+				return session
+					.Execute(query.Bind(tableId))
+					.Select(row => row.GetValue<string>("name"))
+					.ToList();
+			}
+		}
+
+		public List<TableChange> FindChangesets(string tableId, ChangeSearchOptions searchOptions)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public TableChange GetChangesetDetails(string changesetId)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
